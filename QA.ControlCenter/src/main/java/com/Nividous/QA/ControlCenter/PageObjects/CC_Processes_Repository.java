c43f6bc549cece3fbfc94ee44505f84a14653858
Akula.Navya <Akula.Navya@DESKTOP-D52S8GO>
2021-10-16 16:41:29 +0530

    Commiting on 16th oct

--- QA.ControlCenter/src/main/java/com/Nividous/QA/ControlCenter/PageObjects/CC_Processes_Repository.java
+++ QA.ControlCenter/src/main/java/com/Nividous/QA/ControlCenter/PageObjects/CC_Processes_Repository.java
@@ -2,14 +2,29 @@ package com.Nividous.QA.ControlCenter.PageObjects;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.Nividous.QA.ControlCenter.CommonMethods.CommonMethods;
+
+import testBase.Base;
 
 public class CC_Processes_Repository {
 
-	@FindBy ( id = "showScheduler")
-	WebElement uploadDraftProcessBtn;
+	private static CommonMethods common = new CommonMethods();
+	
+	public CC_Processes_Repository()
+
+	{
+		PageFactory.initElements(Base.getDriver(), this);
+	}
+		@FindBy ( id = "showScheduler")
+		WebElement uploadDraftProcessBtn;
 	
 	@FindBy ( id = "uploadFiles")
 	WebElement browseBtn;
+	
+	@FindBy (css = "i[class='glyphicon glyphicon-trash']")
+	WebElement removeBtn;
 	
 	@FindBy ( xpath = "//*[@class='btn']")
 	WebElement backToRepoBtn;
@@ -19,6 +34,7 @@ public class CC_Processes_Repository {
 	
 	@FindBy ( xpath = "//*[@class='fa fa-list-ol']")
 	WebElement listIcon;
+	
 	
 	
 	
--- QA.ControlCenter/src/main/java/com/Nividous/QA/ControlCenter/PageObjects/CC_Robots_Schedule.java
+++ QA.ControlCenter/src/main/java/com/Nividous/QA/ControlCenter/PageObjects/CC_Robots_Schedule.java
@@ -2,8 +2,25 @@ package com.Nividous.QA.ControlCenter.PageObjects;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.Nividous.QA.ControlCenter.CommonMethods.CommonMethods;
+
+import testBase.Base;
 
 public class CC_Robots_Schedule {
+	
+	private static CommonMethods common = new CommonMethods();
+	
+
+	public  CC_Robots_Schedule()
+
+	{
+		PageFactory.initElements(Base.getDriver(), this);
+	}
 	
 	@FindBy(id = "showScheduler")
 	WebElement addScheduleBtn ;
@@ -35,11 +52,47 @@ public class CC_Robots_Schedule {
 	@FindBy(xpath = "//*[@id=\"daily\"]")
 	WebElement scheduleDaily;
 	
+	@FindBy(css = "input[id='recursionDate']")
+	WebElement startsOn;
+	
+	@FindBy(css = "input[id='never']")
+	WebElement expiresOn;
+	
+	@FindBy(css = "input[id='on']")
+	WebElement expiresNever;
+	
+	@FindBy(css = "input[id='dnrDay']")
+	WebElement dnrDays;
+	
+	@FindBy (css="select[id='dnrDaySelect']")
+	WebElement ddDnrDays;
+	
+	@FindBy(css = "input[id='drnDate']")
+	WebElement dnrDates;
+	
+	@FindBy (css = "input[id='fromDateInput']")
+	WebElement dnrDatesfromDate;
+	
+	@FindBy (css="input[id='toDateInput']")
+	WebElement dnrDatesToDate;
+	
+	@FindBy (css = "input[id='RecursionHours']")
+	WebElement perHours;
+	
 	@FindBy(xpath = "//*[@id=\"weekly\"]")
 	WebElement scheduleWeekly;
 	
+	@FindBy (css= "select[id='dayofWeek']")
+	WebElement ddDayOfWeek;
+	
 	@FindBy(xpath = "//*[@id=\"monthly\"]")
 	WebElement scheduleMonthly;
+	
+	@FindBy ( css = "input[id='cron']")
+	WebElement advanceSchedule;
+	
+	@FindBy ( css="input[id='cronExp']")
+	WebElement cronExp;
 	
 	@FindBy(id = "scheduleProcessButton")
 	WebElement scheduleProcessBtn;
@@ -58,5 +111,276 @@ public class CC_Robots_Schedule {
 	
 	@FindBy(id = "FailureReceipients")
 	WebElement additionalReciepients;
+	
+	
+	public void addScheduleNow()
+	{
+		try {
+			common.clickButton(addScheduleBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		try {
+			common.clickButton(ddSelectProcess);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		Select ddProcess = new Select(ddSelectProcess);
+		ddProcess.selectByIndex(1);
+		
+		common.sendKeys(value, "schedule now");
+		common.scrollBarDown("400", 0);
+		try {
+			common.clickButton(scheduleNow);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		try {
+			common.clickButton(scheduleProcessBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void addScheduleOnce()
+	{
+		try {
+			common.clickButton(addScheduleBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		try {
+			common.clickButton(ddSelectProcess);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		Select ddProcess = new Select(ddSelectProcess);
+		ddProcess.selectByIndex(1);
+		
+		common.sendKeys(value, "schedule once");
+		common.scrollBarDown("400", 0);
+		try {
+			common.clickButton(scheduleOnce);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		common.sendKeys(startsOn, "11/10/2021 20:00");//Mention date and time in dd/mm/yyyy hours:minutes(24 hour format)
+		try {
+			common.clickButton(scheduleProcessBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void addScheduleDaily()
+	{
+		try {
+			common.clickButton(addScheduleBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		try {
+			common.clickButton(ddSelectProcess);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		Select ddProcess = new Select(ddSelectProcess);
+		ddProcess.selectByIndex(1);
+		
+		common.sendKeys(value, "schedule Daily");
+		common.scrollBarDown("400", 0);
+		try {
+			common.clickButton(scheduleDaily);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		common.sendKeys(startsOn, "11/10/2021 20:00");//Mention date and time in dd/mm/yyyy hours:minutes(24 hour format)
+		try {
+			common.clickButton(expiresNever);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		try {
+			common.clickButton(dnrDays);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		try {
+			common.clickButton(scheduleProcessBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+	
+	}
+	
+	public void addScheduleHourly()
+	{
+		try {
+			common.clickButton(addScheduleBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		try {
+			common.clickButton(ddSelectProcess);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		Select ddProcess = new Select(ddSelectProcess);
+		ddProcess.selectByIndex(1);
+		
+		common.sendKeys(value, "schedule Hourly");
+		common.scrollBarDown("400", 0);
+		try {
+			common.clickButton(scheduleHourly);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		common.sendKeys(startsOn, "11/10/2021 20:30");//Mention date and time in dd/mm/yyyy hours:minutes(24 hour format)
+		try {
+			common.clickButton(expiresNever);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		common.sendKeys(perHours, "1");
+		try {
+			common.clickButton(dnrDays);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		try {
+			common.clickButton(scheduleProcessBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void addScheduleWeekly()
+	{
+		try {
+			common.clickButton(addScheduleBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		try {
+			common.clickButton(ddSelectProcess);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		Select ddProcess = new Select(ddSelectProcess);
+		ddProcess.selectByIndex(1);
+		
+		common.sendKeys(value, "schedule Weekly");
+		common.scrollBarDown("400", 0);
+		try {
+			common.clickButton(scheduleWeekly);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		common.sendKeys(startsOn, "11/10/2021 21:00");//Mention date and time in dd/mm/yyyy hours:minutes(24 hour format)
+		try {
+			common.clickButton(ddDayOfWeek);
+		} catch (InterruptedException e1) {
+			Thread.currentThread().interrupt();
+			e1.printStackTrace();
+		}
+		Select ddDay = new Select(ddDayOfWeek);
+		ddDay.selectByValue("Wednesday");
+		
+		
+		try {
+			common.clickButton(expiresNever);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		try {
+			common.clickButton(dnrDates);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		common.sendKeys(dnrDatesfromDate, "13/10/2021");
+		common.sendKeys(dnrDatesToDate, "14/10/2021");
+		try {
+			common.clickButton(scheduleProcessBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+	}
+	
+	public void scheduleAdvanced()
+	{
+		try {
+			common.clickButton(addScheduleBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		try {
+			common.clickButton(ddSelectProcess);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		Select ddProcess = new Select(ddSelectProcess);
+		ddProcess.selectByIndex(1);
+		
+		common.sendKeys(value, "schedule Advanced");
+		common.scrollBarDown("400", 0);
+		try {
+			common.clickButton(advanceSchedule);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		common.sendKeys(cronExp, "enter cron expression");
+		common.scrollBarDown("400", 0);
+		try {
+			common.clickButton(scheduleProcessBtn);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+		
+		
+	}
 	
 }
